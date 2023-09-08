ddba6344e817c173f66836a8b5c96a6824bee016
bloater01 <134257050+bloater01@users.noreply.github.com>
2023-09-08 10:25:03 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,11 +1,30 @@
 function displaySum() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
 
   let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  document.getElementById("answer").innerHTML = `${firstNum} + ${secondNum} equals ${total}`;
+}
+
+function displayMultiply() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+
+  let product = firstNum * secondNum;
+  document.getElementById("answer").innerHTML = `${firstNum} × ${secondNum} equals ${product}`;
+}
+
+function displaySubtract() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+
+  let difference = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = `${firstNum} - ${secondNum} equals ${difference}`;
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
+document.getElementById('multiplyButton').addEventListener("click", displayMultiply);
+document.getElementById('subtractButton').addEventListener("click", displaySubtract);
+
 
 
