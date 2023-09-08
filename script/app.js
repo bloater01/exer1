285a01a8c879a1d7699671d4c7956304455452fe
bloater01 <134257050+bloater01@users.noreply.github.com>
2023-09-08 11:13:52 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,23 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('sumButton').addEventListener("click", displaySum);
+    document.getElementById('multiplyButton').addEventListener("click", displayMultiply);
+    document.getElementById('subtractButton').addEventListener("click", displaySubtract);
+});
+
 function displaySum() {
-    let firstNum = Number(document.getElementById('firstNum').innerText);
-    let secondNum = Number(document.getElementById('secondNum').innerText);
+    let firstNum = Number(document.getElementById('firstNum').textContent);
+    let secondNum = Number(document.getElementById('secondNum').textContent);
 
     let total = firstNum + secondNum;
-    document.getElementById("answer").innerText = `The sum is: ${total}`;
+    document.getElementById("answer").textContent = `The sum is: ${total}`;
 }
 
 function displayMultiply() {
-    let firstNum = Number(document.getElementById('firstNum').innerText);
-    let secondNum = Number(document.getElementById('secondNum').innerText);
+    let firstNum = Number(document.getElementById('firstNum').textContent);
+    let secondNum = Number(document.getElementById('secondNum').textContent);
 
     let product = firstNum * secondNum;
-    document.getElementById("answer").innerText = `The product is: ${product}`;
+    document.getElementById("answer").textContent = `The product is: ${product}`;
 }
 
 function displaySubtract() {
-    let firstNum = Number(document.getElementById('firstNum').innerText);
-    let secondNum = Number(document.getElementById('secondNum').innerText);
+    let firstNum = Number(document.getElementById('firstNum').textContent);
+    let secondNum = Number(document.getElementById('secondNum').textContent);
 
     let difference = firstNum - secondNum;
-    document.getElementById("answer").innerText = `The difference is: ${difference}`;
+    document.getElementById("answer").textContent = `The difference is: ${difference}`;
 }
