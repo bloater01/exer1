9cd5813a92df06e6734c7225095589230880af36
bloater01 <134257050+bloater01@users.noreply.github.com>
2023-09-08 10:11:57 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -7,5 +7,23 @@ function displaySum() {
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
+
+function displaySum() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum + secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+}
+
 document.getElementById('multiplyButton').addEventListener("click", displaySum);
+
+function displaySum() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum + secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+}
+
 document.getElementById('subButton').addEventListener("click", displaySum);
